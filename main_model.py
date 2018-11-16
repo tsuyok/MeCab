d36432e41a45fa24c977bfc8bb5edaef55757e81
Tsuyoshi Yasunishi <tsuyok@gmail.com>
2018-11-16 15:55:17 +0900

    コマンド変更

--- main_model.py
+++ main_model.py
@@ -18,15 +18,15 @@ def main(argv=sys.argv):
 
     # テキストからコーパス作成
     print("find " + argv[1] + " | grep wiki | awk '{system(\"cat \"$0\" >> wiki.txt\")}'")
-    subprocess.check_call("find " + argv[1] + " | grep wiki | awk '{system(\"cat \"$0\" >> wiki.txt\")}'")
+    subprocess.check_call("find " + argv[1] + " | grep wiki | awk '{system(\"cat \"$0\" >> wiki.txt\")}'", shell=True)
 
     # MeCab
     print("mecab -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd wiki.txt -o wiki_wakati.txt")
-    subprocess.check_call("mecab -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd wiki.txt -o wiki_wakati.txt")
+    subprocess.check_call("mecab -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd wiki.txt -o wiki_wakati.txt", shell=True)
 
     # 文字コード
     print("nkf -w --overwrite wiki_wakati.txt")
-    subprocess.check_call("nkf -w --overwrite wiki_wakati.txt")
+    subprocess.check_call("nkf -w --overwrite wiki_wakati.txt", shell=True)
 
     # Word2Vec モデル化
     sentences = word2vec.Text8Corpus('./wiki_wakati.txt')
